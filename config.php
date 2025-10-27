79b6758c4a2de087e8c6b1d43e38535e9f553c35
胡博涵 <husibo16@gmail.com>
2025-10-27 23:36:35 +0800

    Add files via upload

--- config.php
+++ config.php
@@ -1,10 +1,14 @@
 <?php
-/*数据库配置*/
+/*数据库配置
+ * 生产环境建议通过环境变量提供数据库连接信息。
+ * 支持以下环境变量：
+ *   DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME, DB_PREFIX
+ */
 $dbconfig=array(
-	'host' => 'localhost', //数据库服务器
-	'port' => 3306, //数据库端口
-	'user' => '', //数据库用户名
-	'pwd' => '', //数据库密码
-	'dbname' => '', //数据库名
-	'dbqz' => 'pay' //数据表前缀
+        'host' => getenv('DB_HOST') ?: 'localhost', //数据库服务器
+        'port' => getenv('DB_PORT') ?: 3306, //数据库端口
+        'user' => getenv('DB_USER') ?: '', //数据库用户名
+        'pwd' => getenv('DB_PASSWORD') ?: '', //数据库密码
+        'dbname' => getenv('DB_NAME') ?: '', //数据库名
+        'dbqz' => getenv('DB_PREFIX') ?: 'pay' //数据表前缀
 );
